92c40ee8af34329ca36a3887ad7435a5d924f602
PAvel Kulik <Kanstantin0105@gmail.com>
2020-07-27 00:25:46 +0300

    task_1_1

---
+++ task_1_1.py
@@ -0,0 +1,6 @@
+a = int(input("Input the first real number: "))
+b = int(input("Input the second real number: "))
+sums = a + b
+difference = a - b
+composition = a * b
+print("sums=", sums, "difference=", difference, "composition=", composition)
